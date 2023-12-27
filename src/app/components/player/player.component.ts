0301251038294eab0c7f03044eb04217459e5eeb
lcarera <lcarera04@gmail.com>
2023-12-27 20:31:12 -0300

    Fix player component bugs and add player service injection

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -1,6 +1,7 @@
-import { Component, ElementRef, ViewChild, signal } from '@angular/core';
+import { Component, ElementRef, ViewChild, effect, inject, signal } from '@angular/core';
 import { Song } from '@assets/data/data';
 import { SvgIconComponent } from 'angular-svg-icon';
+import { PlayerService } from 'src/app/services/player.service';
 
 @Component({
   selector: 'player',
@@ -10,20 +11,31 @@ import { SvgIconComponent } from 'angular-svg-icon';
   styleUrl: './player.component.scss'
 })
 export class PlayerComponent  {
-  public isPlaying = signal(false)
-  public currentSong = signal<Song | null>(null)
+  private playerService = inject(PlayerService)
+  public isPlaying = false
+  public currentSong = this.playerService.getCurrentSong()
   @ViewChild('audioRef') audioRef!: ElementRef;
   audio!: HTMLAudioElement;
+  
+  private changeSongEffect = effect(() => {
+    this.reproduceSong()
+  })
 
   ngAfterViewInit(){
     this.audio = this.audioRef.nativeElement
-    this.audio.src = `/assets/music/1/01.mp3`
+  }
+
+  reproduceSong() {
+    this.audio.src = `/assets/music/${this.currentSong().albumId}/0${this.currentSong().id}.mp3`
+    this.audio.play()
+    this.isPlaying = true
   }
 
   changePlayState() {
-    if(this.isPlaying()) this.audio.pause();
+    if(this.isPlaying) this.audio.pause();
     else this.audio.play();
-    this.isPlaying.update(state => !state)
+    this.isPlaying = !this.isPlaying
   }
+
 }
  
