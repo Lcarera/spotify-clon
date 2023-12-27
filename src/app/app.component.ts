4bb9cd02ec661a3a4a70d014cb9d62e87968ac90
lcarera <lcarera04@gmail.com>
2023-12-26 21:20:45 -0300

    Add player component to footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AsideMenuComponent } from './components/aside-menu/aside-menu.component';
+import { PlayerComponent } from './components/player/player.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AsideMenuComponent],
+  imports: [CommonModule, RouterOutlet, AsideMenuComponent, PlayerComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
