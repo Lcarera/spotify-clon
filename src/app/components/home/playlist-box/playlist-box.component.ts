df4207bea6bc26ced0a88e1ec794bedf53dd79cb
lcarera <lcarera04@gmail.com>
2023-12-27 20:31:02 -0300

    Add dependencies and update method in

--- src/app/components/home/playlist-box/playlist-box.component.ts
+++ src/app/components/home/playlist-box/playlist-box.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
 import { Playlist } from '@assets/data/data';
 import { RouterModule } from '@angular/router';
+import { SvgIconComponent } from 'angular-svg-icon';
+import { PlayerService } from 'src/app/services/player.service';
 @Component({
   selector: 'playlist-box',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, SvgIconComponent],
   templateUrl: './playlist-box.component.html',
   styleUrl: './playlist-box.component.scss'
 })
@@ -12,7 +14,13 @@ export class PlaylistBoxComponent implements OnInit{
   @Input() playlist:Playlist = {} as Playlist;
   artistsString:string = ''
 
+  private playerService = inject(PlayerService)
+
   ngOnInit(): void {
     this.artistsString = new Intl.ListFormat().format(this.playlist.artists);
+  }
+
+  setSong() {
+    this.playerService.setRandomSong(this.playlist.albumId)
   }
 }
